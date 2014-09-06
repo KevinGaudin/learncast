d4807c424cc5f40f3901597af6ac641c7a0f5ade
Kevin Gaudin <kevin.gaudin@gmail.com>
2014-09-07 00:32:14 +0200

    externalize javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
     window.onload = function() {
-      window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-      window.castReceiverManager.start();
+    	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+    	window.castReceiverManager.start();
+
+/*    	window.castReceiverManager.onSenderDisconnected = function(event) {
+    		if (window.castReceiverManager.getSenders().length == 0 &&
+    			event.reason == cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER) {
+    			window.close();
+    		}
+    	}*/
     }
