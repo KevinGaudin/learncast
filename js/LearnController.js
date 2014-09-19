d6474c94dade9bf7a43a64cfabb302b20cf777e6
Kevin Gaudin <kevin.gaudin@gmail.com>
2014-09-19 23:29:08 +0200

    One way JSON messaging

--- js/LearnController.js
+++ js/LearnController.js
@@ -1,19 +1,20 @@
+var LEARN_PROTOCOL = 'urn:x-cast:com.kg.learn';
+
 console.log("Create app module");
 var learnApp = angular.module('LearnApp', []);
 
 var LearnController = function($scope) {
 	var _this = this;
-	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-	bus = window.castReceiverManager.getCastMessageBus('urn:x-cast:com.kg.learn');
-	window.castReceiverManager.start();
 
 
 	$scope.sendersCount = 0;
+	$scope.players = [];
 
 	this.addSender = function(event) {
 		$scope.$apply(function() {
 			console.log("New sender connected");
 			console.log(event);
+			console.log(window.castReceiverManager.getSenders());
 			$scope.sendersCount++;
 			console.log($scope.sendersCount);
 		});
@@ -32,8 +33,24 @@ var LearnController = function($scope) {
 			$scope.sendersCount--;
 		});
 	};
+
+	this.onMessage = function(event) {
+		$scope.$apply(function() {
+			console.log("Message received: ", event);
+			console.log("Add Player: ", event.data.name);
+			$scope.players.push({
+				name: event.data.name
+			});
+			console.log ("players", $scope.players);
+		});
+	};
+
+	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+	this.bus = window.castReceiverManager.getCastMessageBus(LEARN_PROTOCOL,cast.receiver.CastMessageBus.MessageType.JSON);
+	this.bus.onMessage = this.onMessage;
 	window.castReceiverManager.onSenderDisconnected = this.removeSender;
 	window.castReceiverManager.onSenderConnected = this.addSender;
+	window.castReceiverManager.start();
 };
 
 LearnController.$inject = ['$scope'];
