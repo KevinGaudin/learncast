712b9d3a88afe6ad672b2a8e0a4f13cdf30a8177
Kevin Gaudin <kevin.gaudin@gmail.com>
2014-09-21 17:10:33 +0200

    Ask for player identification, receive player identification

--- js/LearnController.js
+++ js/LearnController.js
@@ -3,6 +3,33 @@ var LEARN_PROTOCOL = 'urn:x-cast:com.kg.learn';
 console.log("Create app module");
 var learnApp = angular.module('LearnApp', []);
 
+function Player(senderId, channel) {
+	this.senderId = senderId;
+	this.channel = channel;
+	this.channel.onMessage = this.onMessage.bind(this);
+	this.channel.send({command: "identify"});
+};
+
+Player.prototype = {
+	senderId: null,
+	name: null,
+	channel: null,
+	onMessage: function(event) {
+		console.log("Received message from: " + this.senderId + ": ", event);
+		switch(event.message.command) {
+			case 'identify':
+				console.log (this.senderId + " gave name: " + event.message.name);
+				this.name = event.message.name;
+				break;
+			default:
+				console.log("unknown command");
+		}
+	}
+}
+
+/**
+ * LearnController : main controller for our AngularJS app
+ */
 var LearnController = function($scope) {
 	var _this = this;
 
@@ -15,7 +42,16 @@ var LearnController = function($scope) {
 			console.log("New sender connected");
 			console.log(event);
 			console.log(window.castReceiverManager.getSenders());
-			$scope.sendersCount++;
+
+			console.log("Get Channel with " + event.senderId);
+
+			var player = new Player(event.senderId, _this.bus.getCastChannel(event.senderId));
+
+
+			$scope.players.push(player);
+			console.log("players", $scope.players);
+
+			$scope.sendersCount = window.castReceiverManager.getSenders().length;
 			console.log($scope.sendersCount);
 		});
 	}
@@ -30,23 +66,22 @@ var LearnController = function($scope) {
 				event.reason == cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER) {
 				window.close();
 			}
-			$scope.sendersCount--;
+			$scope.sendersCount = window.castReceiverManager.getSenders().length;
 		});
 	};
 
+	/**
+	 * Called when any message is received. Messages from multiple senders are better handled from
+	 * channels, though. 
+	 **/
 	this.onMessage = function(event) {
 		$scope.$apply(function() {
-			console.log("Message received: ", event);
-			console.log("Add Player: ", event.data.name);
-			$scope.players.push({
-				name: event.data.name
-			});
-			console.log ("players", $scope.players);
+			console.log("Global message received: ", event);
 		});
 	};
 
 	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-	this.bus = window.castReceiverManager.getCastMessageBus(LEARN_PROTOCOL,cast.receiver.CastMessageBus.MessageType.JSON);
+	this.bus = window.castReceiverManager.getCastMessageBus(LEARN_PROTOCOL, cast.receiver.CastMessageBus.MessageType.JSON);
 	this.bus.onMessage = this.onMessage;
 	window.castReceiverManager.onSenderDisconnected = this.removeSender;
 	window.castReceiverManager.onSenderConnected = this.addSender;
