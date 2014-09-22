689a5db7ce8afbc03801c65c4b2487c799be76dd
Kevin Gaudin <kevin.gaudin@gmail.com>
2014-09-22 20:57:21 +0200

    Asynchronous player questions flow OK

--- js/LearnController.js
+++ js/LearnController.js
@@ -8,24 +8,62 @@ function Player(senderId, channel) {
 	this.senderId = senderId;
 	this.channel = channel;
 	this.channel.onMessage = this.onMessage.bind(this);
-	this.channel.send({command: "identify"});
+	this.channel.send({
+		command: "identify"
+	});
 };
 
 Player.prototype = {
 	senderId: null,
 	name: null,
 	channel: null,
+	readyToPlay: false,
+	questions: null,
+	currentQuestion: 0,
+	scorecard: [],
 	onMessage: function(event) {
 		console.log("Received message from: " + this.senderId + ": ", event);
-		switch(event.message.command) {
+		switch (event.message.command) {
 			case 'identify':
-				console.log (this.senderId + " gave name: " + event.message.name);
+				console.log(this.senderId + " gave name: " + event.message.name);
 				this.name = event.message.name;
+				break;
+			case 'readyToPlay':
+				this.readyToPlay = event.message.value;
+				console.log(this.name + " ready? ", event.message.value);
+				break;
+			case 'submitAnswer':
+				console.log(this.name + " submits " + event.message.value);
+				this.checkAnswer(event.message.value);
 				break;
 			default:
 				console.log("unknown command");
 		}
+	},
+	ask: function() {
+		var command = {
+			command: 'question',
+			question: this.questions[this.currentQuestion].question
+		};
+		console.log("Ask " + this.senderId + " question", command);
+		this.channel.send(command);
+	},
+	setQuestions: function(questions) {
+		this.questions = questions;
+	},
+	checkAnswer: function(answer) {
+		if (answer == this.questions[this.currentQuestion].result) {
+			console.log(this.name + " " + answer + " is the right answer!");
+			this.scorecard.push("1");
+			this.currentQuestion++;
+			if (this.currentQuestion < this.questions.length) {
+				this.ask();
+			}
+		} else {
+			console.log(this.name + " " + answer + " is wrong, try again...");
+		}
 	}
+
 }
 
 /**
@@ -73,11 +111,33 @@ var LearnController = function($scope, QuestionFactory) {
 
 	/**
 	 * Called when any message is received. Messages from multiple senders are better handled from
-	 * channels, though. 
+	 * channels, though.
 	 **/
 	this.onMessage = function(event) {
 		$scope.$apply(function() {
 			console.log("Global message received: ", event);
+			switch (event.data.command) {
+				case 'readyToPlay':
+					// First check that everybody is ready
+					console.log("Are all players ready ?")
+					var allPlayersReady = true;
+					for (var i = 0; i < $scope.players.length; i++) {
+						if (!$scope.players[i].readyToPlay) {
+							allPlayersReady = false;
+							break;
+						}
+					}
+
+					if (allPlayersReady) {
+						console.log("Let's start the game");
+						_this.startGame();
+					} else {
+						console.log("Still waiting for other players");
+					}
+					break;
+				default:
+					console.log("No global action required");
+			}
 		});
 	};
 
@@ -88,7 +148,24 @@ var LearnController = function($scope, QuestionFactory) {
 	window.castReceiverManager.onSenderConnected = this.addSender;
 	window.castReceiverManager.start();
 
+	this.questions = [];
+	this.startGame = function() {
+
+		// Prepare 10 questions
+		for (var i = 0; i < 10; i++) {
+			_this.questions[i] = QuestionFactory.getQuestion();
+		}
+
+		$scope.players.forEach(function(player, iPlayer, players) {
+			player.setQuestions(_this.questions);
+			player.ask();
+		});
+
+	}
+
 };
+
+
 
 LearnController.$inject = ['$scope', 'QuestionFactory'];
 learnApp.controller('LearnController', LearnController);
